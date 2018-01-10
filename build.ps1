ba3c222e8f7a00bf8bbf5b8211b5e35e0e4ebff6
Shawn Neal <sneal@sneal.net>
2018-01-10 10:16:37 -0800

    Add auto versioning

--- build.ps1
+++ build.ps1
@@ -3,16 +3,20 @@
     Builds the CF CLI Chocolatey package
 .Description
     Using the version specified, this script will create a Choco package ready
-    to publish to Chocolate.org. This script will _not_ automatically push the
+    to publish to Chocolate.org. This script will automatically push the
     build artifacts.
 #>
 
-param (
-    [Parameter(Mandatory=$true)][string]$version
-)
-
 $buildDir = $pwd.Path
 $tplDir = Join-Path -Path $buildDir -ChildPath 'template'
+
+# Get the latest GH release and choco version
+$chocoVersion = ((choco info cloudfoundry-cli -r) -split '\|')[1]
+$version = (Invoke-WebRequest -UseBasicParsing 'https://api.github.com/repos/cloudfoundry/cli/releases/latest' |`
+  ConvertFrom-Json | Select-Object -ExpandProperty tag_name).Replace('v', '')
+
+Write-Output "Chocolatey CF CLI version: $chocoVersion"
+Write-Output "GitHub Releases CF CLI version: $version"
 
 # Write out nuspec file, force UTF-8 without BOM
 $nuspecContent = (Get-Content -Encoding utf8 -Raw "$tplDir\cloudfoundry-cli.nuspec.tpl").
@@ -23,7 +27,7 @@ $utf8NoBom = New-Object System.Text.UTF8Encoding
 
 # Write out the install PS script
 $toolsDir = Join-Path -Path $buildDir -ChildPath 'tools'
-New-Item -Path $toolsDir -ItemType Directory -Force
+New-Item -Path $toolsDir -ItemType Directory -Force | Out-Null
 (Get-Content -Encoding utf8 -Raw "$tplDir\chocolateyinstall.ps1.tpl").
   Replace('${version}', $version) | `
   Set-Content "$toolsDir\chocolateyinstall.ps1" -Encoding utf8
@@ -35,6 +39,10 @@ if (-not (Test-Path env:ChocolateyInstall)) {
 $chocoBinDir = Join-Path -Path $env:ChocolateyInstall -ChildPath 'bin'
 Invoke-Expression "$chocoBinDir\cpack.exe $buildDir\cloudfoundry-cli.nuspec"
 
-Write-Output ''
-Write-Output 'To push the build artifact to Chocolatey.org run:'
-Write-Output "choco push $buildDir\cloudfoundry-cli.$version.nupkg"
+# Only publish the choco package if it's out of date
+if ([System.Version]$version -gt [System.Version]$chocoVersion) {
+  Write-Output 'Publishing CF CLI to Chocolatey...'
+  choco push $buildDir\cloudfoundry-cli.$version.nupkg
+} else {
+  Write-Output 'The latest CF CLI version is already available on Chocolatey, skipping publish'
+}
